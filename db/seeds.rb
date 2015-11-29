4cf12afd755fd92156de3736748f7f37f3ce9555
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-11-29 10:55:09 -0600

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(message: "lorem ipsum",created_time: "2015-11-29T15:56:03+0000",f_id: "1651286195118435_1673502692896785")
+Post.create(message: "Memetroid II: The Return of Pepe added a new photo.",created_time: "2015-11-29T00:13:30+0000",f_id: "1651286195118435_1673351739578547")
