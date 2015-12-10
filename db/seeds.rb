b2ca8a7cedd313c442c52ef20163419d4e3a5d4f
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-12-10 12:51:20 -0600

    Update seeds.rb
    ass

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Post.create(message: "lorem ipsum",created_time: "2015-11-29T15:56:03+0000",f_id: "1651286195118435_1673502692896785")
-Post.create(message: "Memetroid II: The Return of Pepe added a new photo.",created_time: "2015-11-29T00:13:30+0000",f_id: "1651286195118435_1673351739578547")
+Post.create(message: "Hola quiero saber cunto cuestan? Me interesa un peluche de pinkie",created_time: "2015-12-10T06:01:47+0000",f_id: "535786039771317_1222261191123795")
+
 
